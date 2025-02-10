4588fa51ed1fbac552700670a358b0230d362092
Mahan Mehrabi <gnusmhn@gmail.com>
2025-02-10 12:31:06 +0330

    Update index.js

--- 04.Variables/index.js
+++ 04.Variables/index.js
@@ -15,12 +15,10 @@ let age = 17;
 let price = 99.99;
 let gpa = 2.5;
 
-
 //To use variables in sentences we put ${} and then we insert the variable name in the curly brackets
 console.log(`I am ${age} years old`);
 console.log(`The price is $${price}`);
 console.log(`Your gpa is ${gpa}`);
-
 
 //2) string variables
 
@@ -32,16 +30,13 @@ console.log(`My name is ${firstName}`);
 console.log(`My favourite food is ${favoriteFood}`);
 console.log(`Your email is ${email}`);
 
-
 //3) boolean variables
-
 
 //Boolean variables return "true" or "false" 
 
 let forSale = true;
 let isOnline = false;
 let isStudent = true;
-
 
 console.log(`Is this car for sale ? ${forSale}`);
 console.log(`secondGuy is online : ${isOnline}`);
