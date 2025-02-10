c501c3fcc537d11d24be014a1d5040b1deff6aef
Mahan Mehrabi <gnusmhn@gmail.com>
2025-02-10 12:33:28 +0330

    Add files via upload

---
+++ 05.Data type of a variable/index.js
@@ -0,0 +1,9 @@
+let num = 12345;
+let name = "something";
+let enrolled = true;
+
+//to show a variable datatype we use "typeof" and then the name of the variable
+
+console.log(typeof num);
+console.log(typeof name);
+console.log(typeof enrolled);
