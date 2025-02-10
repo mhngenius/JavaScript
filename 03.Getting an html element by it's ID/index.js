ebb592be3712e0e7cb64531b455ce2df225ac419
Mahan Mehrabi <gnusmhn@gmail.com>
2025-02-10 12:25:33 +0330

    Add files via upload

---
+++ 03.Getting an html element by it's ID/index.js
@@ -0,0 +1,5 @@
+//By using document class and the getElementById method , you can get an html element by its id and do some actions to them , here is a simple text content action.
+
+
+document.getElementById("myH1").textContent = `Hello World!`;
+document.getElementById("myP").textContent = `I like coding`;
