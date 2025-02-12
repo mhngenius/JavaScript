be1de70a6fc7a627e3b4f897650a0beee7f3da36
Mahan Mehrabi <gnusmhn@gmail.com>
2025-02-12 11:45:01 +0330

    Add files via upload

---
+++ 07.Function (onclick method)/index.js
@@ -0,0 +1,8 @@
+//first of all we get the html ids and put them in a variable
+
+//We use "onclick" method to create a function for a button and we put the action that we want from the button to do in "function"
+
+let myBtn = document.getElementById("myBtn").onclick = function () {
+    console.log(`Hello World!`)
+}
+
