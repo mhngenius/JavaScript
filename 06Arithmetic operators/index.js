f55c868d6a7b3987ee1dcfb1a3ce061490bcde2c
Mahan Mehrabi <gnusmhn@gmail.com>
2025-02-12 11:34:48 +0330

    Add files via upload

---
+++ 06Arithmetic operators/index.js
@@ -0,0 +1,23 @@
+//Arithmetic operators (mathematical actions)
+
+let students = 30;
+
+students = students + 1;
+students = students - 1;
+students = students * 2;
+students = students / 2;
+students = students ** 2;
+let extraStudents = students % 2;
+console.log(extraStudents);
+
+students += 1;
+students -= 1;
+students *= 2;
+students /= 2;
+students **= 2;
+students %= 2;
+
+students++;
+students--;
+
+console.log(students);
