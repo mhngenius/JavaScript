2d4c51606aff2ca5597b2c72522f5e2bc7cd0b85
Mahan Mehrabi <gnusmhn@gmail.com>
2025-02-12 11:47:37 +0330

    Update index.js

--- 03.Getting an html element by it's ID/index.js
+++ 03.Getting an html element by it's ID/index.js
@@ -1,5 +1,5 @@
 //By using document class and the getElementById method , you can get an html element by its id and do some actions to them , here is a simple text content action.
-
+//textContent method changes the value of the element
 
 document.getElementById("myH1").textContent = `Hello World!`;
 document.getElementById("myP").textContent = `I like coding`;
