6a8bac1394494447ef59a48689fd1ea7788edf5a
Mahan Mehrabi <gnusmhn@gmail.com>
2025-02-12 12:01:01 +0330

    Add files via upload

---
+++ 10.Type conversion/index.js
@@ -0,0 +1,42 @@
+//Type conversion
+
+//1)String to number
+
+
+let age = window.prompt("How old are you?");
+
+age = Number(age);
+
+age += 1;
+
+console.log(age, typeof age);
+
+
+//convert errors
+
+
+// let x = "pizza";
+// let y = "pizza";
+// let z = "pizza";
+
+// let x = "0";
+// let y = "0";
+// let z = "0";
+
+// let x = "";
+// let y = "";
+// let z = "";
+
+
+// let x;
+// let y;
+// let z;
+
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
