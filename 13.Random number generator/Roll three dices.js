2ce1fe91bcc09ab223b77a7eda3cdebaea080478
Mahan Mehrabi <gnusmhn@gmail.com>
2025-02-13 22:30:57 +0330

    Add files via upload

---
+++ 13.Random number generator/Roll three dices.js
@@ -0,0 +1,21 @@
+//Roll 3 dices
+
+
+const myButton = document.getElementById("myButton");
+const Label1 = document.getElementById("Label1");
+const Label2 = document.getElementById("Label2");
+const Label3 = document.getElementById("Label3");
+let min = 1;
+let max = 6;
+let randomNum1;
+let randomNum2;
+let randomNum3;
+
+myButton.onclick = function () {
+    randomNum1 = Math.floor(Math.random() * max) + min;
+    randomNum2 = Math.floor(Math.random() * max) + min;
+    randomNum3 = Math.floor(Math.random() * max) + min;
+    Label1.textContent = randomNum1;
+    Label2.textContent = randomNum2;
+    Label3.textContent = randomNum3;
+}
