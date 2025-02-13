ab804becde9e261d8b7492e3985f5f3c26b29879
Mahan Mehrabi <gnusmhn@gmail.com>
2025-02-13 22:13:34 +0330

    Add files via upload

---
+++ 12.Math objects/index.js
@@ -0,0 +1,66 @@
+//Math objects
+
+//PI & E
+console.log(Math.PI);
+console.log(Math.E);
+
+let x = 3.21;
+let x2 = 3.99;
+let x3 = 3;
+let x4 = 81;
+let x5 = 10;
+let x6 = 45;
+let x7 = -3.21;
+let y = 2;
+let z;
+let z2 = 1;
+
+//Round
+z = Math.round(x);
+console.log(z);
+
+//Round down
+z = Math.floor(x2);
+console.log(z);
+
+//Round up
+z = Math.ceil(x);
+console.log(z);
+
+//Remove decimal points
+z = Math.trunc(x);
+console.log(z);
+
+//Power
+z = Math.pow(x3, y);
+console.log(z);
+
+//Square root
+z = Math.sqrt(x4);
+console.log(z);
+
+//Log
+z = Math.log(x5);
+console.log(z);
+
+//sin / cos / tan
+z = Math.sin(x6);
+console.log(z);
+z = Math.cos(x6);
+console.log(z);
+z = Math.tan(x6);
+console.log(z);
+
+//absoloute
+z = Math.abs(x7);
+console.log(z);
+
+//sign
+z = Math.sign(x7);
+console.log(z);
+
+//max / min
+let max = Math.max(x, y, z2);
+console.log(max);
+let min = Math.min(x, y, z2);
+console.log(min);
