cfcdc5b85cc0a30c1f4f31db7bec4bdea7bb66a7
Mahan Mehrabi <gnusmhn@gmail.com>
2025-02-13 22:24:32 +0330

    Add files via upload

---
+++ 13.Random number generator/index.js
@@ -0,0 +1,18 @@
+//Random number generator practice 
+
+// Roll a dice
+
+
+const myButton = document.getElementById("myButton");
+const myLabel = document.getElementById("myLabel");
+let min = 1;
+let max = 6;
+let randomNum;
+
+myButton.onclick = function () {
+    randomNum = Math.floor(Math.random() * max) + min;
+    myLabel.textContent = randomNum;
+}
+
+
+
