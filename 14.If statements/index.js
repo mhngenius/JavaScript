083aa76f0dddccb485cdfc2216269061700be579
Mahan Mehrabi <gnusmhn@gmail.com>
2025-02-14 18:19:05 +0330

    Add files via upload

---
+++ 14.If statements/index.js
@@ -0,0 +1,29 @@
+//If statements
+
+let age = 25;
+
+//To define an "if" statement , we write if (){} , in "()" we write the condition and in "{}" we write the action that we want from the statement to do if condition was true
+
+if (age >= 18) {
+    console.log(`You are old enough to enter this site.`);
+}
+else {
+    console.log(`You must be 18+ to enter this site.`);
+}
+
+let time = 14;
+if (time < 12) {
+    console.log(`Good Morning!`);
+}
+else {
+    console.log(`Good Afternoon!`);
+}
+
+let isStudent = true;
+
+if (isStudent) {
+    console.log(`You are a student!`);
+}
+else {
+    console.log(`You are NOT a student`);
+}
