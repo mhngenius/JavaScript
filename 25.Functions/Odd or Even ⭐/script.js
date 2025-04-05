b09ef1101242a3d2cd5a4092fd962692aac27d4f
Mahan Mehrabi <gnusmhn@gmail.com>
2025-04-05 21:50:38 +0330

    Add files via upload

---
+++ 25.Functions/Odd or Even ⭐/script.js
@@ -0,0 +1,24 @@
+//function return
+
+function add(x, y) {
+    let result = x + y; //return x + y;
+    return result;
+}
+
+let answer = add(2, 3); //console.log(add(2,3));
+console.log(answer);
+
+function subtract(x, y) {
+    return x - y;
+}
+console.log(subtract(2, 3));
+
+function multiply(x, y) {
+    return x * y;
+}
+console.log(multiply(2, 3))
+
+function divide(x, y) {
+    return x / y;
+}
+console.log(divide(2, 3));
