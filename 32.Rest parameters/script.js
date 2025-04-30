b136315c1eb328e913af2921f7cc3d09eda81764
Mahan Mehrabi <gnusmhn@gmail.com>
2025-04-30 16:01:00 +0330

    Update script.js

--- 32.Rest parameters/script.js
+++ 32.Rest parameters/script.js
@@ -1,7 +1,7 @@
 //Rest parameters = (...rest) allow a function work with a variable
 //                  number of arguments by bundling them into an array
 
-//                  spread = espands an array into seprate elements
+//                  spread = expands an array into seprate elements
 //                  rest = bundles seperate elements into an array
 
 
