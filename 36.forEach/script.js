82f54fc76b10e9f7c19d404af030df87b1b17127
Mahan Mehrabi <gnusmhn@gmail.com>
2025-05-12 15:00:45 +0330

    Add files via upload

---
+++ 36.forEach/script.js
@@ -0,0 +1,56 @@
+//forEach() = a method use tom iterate over the elements of an array and apply a specified function(callback) to each element
+
+let numbers = [1, 2, 3, 4, 5];
+
+//numbers.forEach(double);
+//numbers.forEach(triple);
+//numbers.forEach(square);
+numbers.forEach(cube);
+numbers.forEach(display)
+
+function double(element, index, array) {
+    array[index] = element * 2;
+}
+
+function triple(element, index, array) {
+    array[index] = element * 3;
+}
+
+function square(element, index, array) {
+    array[index] = Math.pow(element, 2)
+}
+
+function cube(element, index, array) {
+    array[index] = Math.pow(element, 3)
+}
+
+function display(element) {
+    console.log(element);
+}
+
+//exercise
+
+
+let fruits = ['apple', 'orange', 'banana', 'coconut'];//let fruits = ['APPLE', 'ORANGE', 'BANANA', 'COCONUT'];
+
+// fruits.forEach(upperCase);
+fruits.forEach(capitalize);
+//fruits.forEach(lowerCase);
+fruits.forEach(display);
+
+function upperCase(element, index, array) {
+    array[index] = element.toUpperCase();
+}
+
+// function lowerCase(element, index, array) {
+//     array[index] = element.toLowerCase();
+// }
+
+function capitalize(element, index, array) {
+    array[index] = element.charAt(0).toUpperCase() + element.slice(1);
+}
+
+
+function display(element) {
+    console.log(element);
+}
