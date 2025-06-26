b9c520766bb6c6c83dfe002949a06a43b1f7efe1
Mahan Mehrabi <gnusmhn@gmail.com>
2025-06-26 15:28:37 +0330

    Add files via upload

---
+++ 43.this/script.js
@@ -0,0 +1,43 @@
+// "this" refers to the object that is calling the method.
+// The value of "this" depends on the context in which it is used.
+// Arrow functions do NOT bind their own "this", so we use regular functions for object methods.
+
+// Creating an object named person1
+const person1 = {
+    name: "Spongebob",             // A property called name
+    favFood: "hamburgers",         // A property called favFood
+
+    // A method to say hello
+    sayHello: function () {
+        // "this.name" refers to person1.name
+        console.log(`Hi! I am ${this.name}`);
+    },
+
+    // A method to simulate eating
+    eat: function () {
+        // Accessing both properties using "this"
+        console.log(`${this.name} is eating ${this.favFood}`);
+    },
+};
+
+// Call methods on person1
+person1.sayHello(); // Output: Hi! I am Spongebob
+person1.eat();      // Output: Spongebob is eating hamburgers
+
+// Creating another object named person2
+const person2 = {
+    name: "Patrick",           // New name
+    favFood: "pizza",          // New favorite food
+
+    // The same methods reused with regular functions
+    sayHello: function () {
+        console.log(`Hi! I am ${this.name}`);
+    },
+
+    eat: function () {
+        console.log(`${this.name} is eating ${this.favFood}`);
+    },
+};
+
+// Call a method on person2
+person2.eat(); // Output: Patrick is eating pizza
